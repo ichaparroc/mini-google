9c20e5f2455baef0eb6e6ed8bf77903bdbd40406
Victor <suryavf@gmail.com>
2018-08-30 01:50:24 -0500

    Fixed bugs.

--- main_i.cpp
+++ main_i.cpp
@@ -2,7 +2,11 @@
 #include<stdio.h>
 #include<string.h>
 #include<iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include<vector>
+#include <omp.h>
 #include"radix_trie.h"
 
 using namespace std;
@@ -10,6 +14,36 @@ using namespace std;
 int main(void)
 {
 	radix_trie_node * master = new radix_trie_node;
+
+	ifstream file;
+	string line;
+	string word;
+
+	string palabra;
+	int      id;
+	float value;
+
+	int i = 0;
+
+	file.open("foodForTrie.txt");
+	while ( getline(file,line) && i<1000000 ){
+
+		istringstream iss(line);
+		getline(iss, word,'\t');
+        palabra = word;
+		getline(iss, word,'\t');
+		id = stoi(word);
+		getline(iss, word,'\t');
+		value = stof(word);
+
+		//if( i%100000 == 0) 
+		cout << ++i << endl ;
+
+		master->insert(palabra,id,value);
+	}
+	file.close();
+
+/*
 	master->insert("doge",102,102);
 	master->insert("dog",101,102);
 	master->insert("doctor",103,104);
@@ -20,12 +54,16 @@ int main(void)
 	master->insert("dacia",1,2);
 	master->insert("dacia",2,6);
 	master->insert("dacia",3,2);
-
-	master->print_radix_trie_node();
+*/
+	//master->print_radix_trie_node();
 
 	vector<string> query;
-	query.push_back("dogo");
-	query.push_back("dacia");
+	double start_time = omp_get_wtime();
+	query.push_back("iglesia");
+	query.push_back("nieves");
+	double _time = omp_get_wtime() - start_time;
+
+    std::cout << "Tiempo de ejecucion: " << _time << std::endl; 
 
 	vector<radix_trie_index*> result_vector;
 	radix_trie_index *result_index;
@@ -34,13 +72,19 @@ int main(void)
 	{
 		result_index=master->search(query[i]);
 		if(result_index!=NULL)
+		{
+			result_index->print_radix_trie_index();
 			result_vector.push_back(result_index);
+		}
 	}
 
 	result_index=AND_results(result_vector);
 
 	cout<<endl<<"---"<<endl;
-	result_index->print_radix_trie_index();
+	if(result_index!=NULL)
+		result_index->print_radix_trie_index();
+	else
+		cout<<"no se han encontrados resultados";
 
 	return 0;
 }
